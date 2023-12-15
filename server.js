c35f3309b5f9fdaf6968237c9e13781df8037924
Jared Collier <collierjared@gmail.com>
2023-12-15 11:27:42 -0700

    create routes to front end files

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+let app = express()
+
+//showing how we derived the full path to our HTML file
+// console.log(import.meta.url)
+// console.log(fileURLToPath(import.meta.url))
+// console.log(dirname(fileURLToPath(import.meta.url)))
+// console.log(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
+
+app.get('/', (req, res) => {
+    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/styles.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/main.js'))
+})
+
+app.listen(8080, () => {
+    console.log('we have started the server on port 8080')
+})
