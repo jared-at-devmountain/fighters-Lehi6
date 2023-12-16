303b4501fa3c7634eb80d4a7ff65987a04b48a3f
Jared Collier <collierjared@gmail.com>
2023-12-15 17:38:20 -0700

    adds GET and DELETE requests, adds DOM manipulation func, uses Express.static()

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@ let createForm = document.getElementById('create-form')
 let createNameInput = document.getElementById('create-name-input')
 let createPowerInput = document.getElementById('create-power-input')
 let createHealthInput = document.getElementById('create-health-input')
+let deleteForm = document.getElementById('delete-form')
+let deleteByNameInput = document.getElementById('delete-by-name-input')
 
 createForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -24,10 +26,64 @@ createForm.addEventListener('submit', (event) => {
 
     axios.post('/create-fighter', maBod)
     .then((response) => {
-        console.log(response.data)
+        loadFightersToDom(response.data)
     })
     .catch((error) => {
         console.log(error)
     })
 
 })
+
+deleteForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let name = deleteByNameInput.value
+
+    axios.delete('/delete-fighter?name=' + name)
+    .then((response) => {
+        loadFightersToDom(response.data)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
+
+axios.get('/fighters')
+.then((result) => {
+    loadFightersToDom(result.data)
+})
+.catch((error) => {
+    console.log(error)
+})
+
+
+
+function loadFightersToDom(fightersArray) {
+    //start with a clean slate: wipe all fighters out of the DOM
+    //before we put new ones in
+    document.getElementById('fighters-display').innerHTML = ''
+
+    for (let i = 0; i < fightersArray.length; i++) {
+        //I'm gonna need to make 4 new elements
+        let containerDiv = document.createElement('div')
+        let heading = document.createElement('h3')
+        let powerP = document.createElement('p')
+        let healthP = document.createElement('p')
+
+        //three of those will need to go in the one
+        containerDiv.appendChild(heading)
+        containerDiv.appendChild(powerP)
+        containerDiv.appendChild(healthP)
+
+        //three of them will need to have text based
+        //off of the particular fightersArray object
+        heading.innerHTML = fightersArray[i].name
+        powerP.innerHTML = 'Power: ' + fightersArray[i].power
+        healthP.innerHTML = 'Health ' + fightersArray[i].health
+
+        //and then last of all, I'll need to put that
+        //one that contains the three in the DOM
+        //as a child of my fighters-display div
+        document.getElementById('fighters-display').appendChild(containerDiv)
+    }
+}
--- server.js
+++ server.js
@@ -14,21 +14,42 @@ let db = []
 // console.log(dirname(fileURLToPath(import.meta.url)))
 // console.log(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
 
-app.get('/', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
-})
+//this replaces the commented out get routes below
+app.use(express.static(join(dirname(fileURLToPath(import.meta.url)), '/public')))
 
-app.get('/css', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/styles.css'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/index.html'))
+// })
 
-app.get('/js', (req, res) => {
-    res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/main.js'))
+// app.get('/css', (req, res) => {
+//     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/styles.css'))
+// })
+
+// app.get('/js', (req, res) => {
+//     res.sendFile(join(dirname(fileURLToPath(import.meta.url)), '/public/main.js'))
+// })
+
+app.get('/fighters', (req, res) => {
+    res.status(200).send(db)
 })
 
 app.post('/create-fighter', (req, res) => {
     db.push(req.body)
-    res.send(db)
+    res.status(200).send(db)
+})
+
+app.delete('/delete-fighter', (req, res) => {
+    let name = req.query.name
+
+    //search through my array and delete any fighters with a name that is the name variable
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].name === name) {
+            db.splice(i, 1)
+            i--
+        }
+    }
+
+    res.status(200).send(db)
 })
 
 app.listen(8080, () => {
